9ac67ae6c267ffb1476ee6073f3e752821e3b917
JuanDanielGonzalez4 <juandanielgonzalez004@gmail.com>
2023-11-28 18:08:42 -0500

    BPM calcultation added

--- main/adc.h
+++ main/adc.h
@@ -3,6 +3,7 @@
 #include "freertos/queue.h"
 #include "esp_log.h"
 #include "esp_adc/adc_oneshot.h"
+#include "esp_timer.h"
 #include "math.h"
 
 #define EXAMPLE_ADC_ATTEN ADC_ATTEN_DB_11
@@ -12,6 +13,9 @@
 #define VOLTAGE_REFERENCE 3300
 #define ADC_MAX_VALUE 4096
 
+
+#define BPM_TRESHOLD 3000
+#define BPM_ARRAY_SIZE 500
 void adc_config(void);
 
 void adc_read_task();
